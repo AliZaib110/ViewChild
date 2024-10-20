2b9bbade8dcad9dd5e286865d86c09f85e9ca1df
alizaib110 <alizaibbuttar9@gmail.com>
2024-10-20 23:16:16 +0500

    set routing environment

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.css'
+  styleUrl: './parent.component.css',
 })
-export class ParentComponent {
-
-}
+export class ParentComponent {}
